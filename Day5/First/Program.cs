76dfa4d9c622754ee2f1c2314738dc4c9df53f95
Mateusz Margos <mateuszmargos@gmail.com>
2018-12-05 20:54:23 +0100

    Day 5 first puzzle solution

---
+++ Day5/First/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputLines;
+            using (var sr = new StreamReader("input.txt"))
+            {
+                var input = sr.ReadToEnd();
+                inputLines = input.Split("\n");
+            }
+
+            var processedPolymer = ProcessPolymer(inputLines[0]);
+            Console.WriteLine(processedPolymer.Length.ToString());
+        }
+
+        public static string ProcessPolymer(string polymer)
+        {
+            bool polymerChanged = true;
+            string currentPolymer = polymer;
+
+            while (polymerChanged)
+            {
+                int initialCharCount = currentPolymer.Length;
+                char little = 'a';
+                char upper = 'A';
+                for (int i = 0; i <= 25; i++)
+                {
+                    bool innerPolymerChanged = true;
+                    while (innerPolymerChanged)
+                    {
+                        int polymerCharCounts = currentPolymer.Length;
+                        var match = new string(new char[] {(char)(little + i), (char)(upper + i)});
+                        currentPolymer = currentPolymer.Replace(match, string.Empty);
+                        match = new string(new char[] {(char)(upper + i), (char)(little + i)});
+                        currentPolymer = currentPolymer.Replace(match, string.Empty);
+                        innerPolymerChanged = polymerCharCounts != currentPolymer.Length;
+                    }
+                }
+                polymerChanged = initialCharCount != currentPolymer.Length;
+            }
+
+            return currentPolymer;
+        }
+    }
+}
